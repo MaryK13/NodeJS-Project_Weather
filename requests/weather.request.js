c9f4c3ba530b5d9a00b0ba2c0a7b7e61b2871fb9
MaryK <jane.gacy@icloud.com>
2021-02-16 15:49:59 +0300

    All files added

---
+++ requests/weather.request.js
@@ -0,0 +1,34 @@
+const rp = require('request-promise')
+
+module.exports = async function(city = '') {
+    if (!city) {
+        throw new Error('Имя города не может быть пустым')
+    }
+
+    const KEY = 'e11467d5d65da67d7ed98d774d60673a'
+    const uri = 'http://api.openweathermap.org/data/2.5/weather'
+    const options = {
+        uri,
+        qs: {
+            appid: KEY,
+            q: city,
+            units: 'imperial',
+        },
+        json: true
+    }
+
+    try {
+        const data = await rp(options)
+        const celsius = (data.main.temp - 32) * 5/9
+
+        return {
+            weather: `${data.name}: ${celsius.toFixed(0)}`,
+            error: null
+        }
+    } catch (error) {
+        return {
+            weather: null,
+            error: error.error.message
+        }
+    }
+}
